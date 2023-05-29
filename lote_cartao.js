dcd834af0ab6f75a495d1ec159f66a94820f72b8
kaue ochner <79806265+kakoch@users.noreply.github.com>
2023-05-29 13:01:24 -0300

    adding a new page to register batches of cards.
    adding a new page to register batches of cards.

---
+++ lote_cartao.js
@@ -0,0 +1,9 @@
+const numero_lote = document.getElementById("numero_lote")
+const fornecedor = document.getElementById("fornecedor")
+const nome_cartao = document.getElementById("nome_cartao")
+const gramatura = document.getElementById("gramatura")
+const formato = document.getElementById("formato")
+const of = document.getElementById("of")
+const numero_bobinas = document.getElementById("numero_bobinas")
+const sub_form = document.getElementById("sub_form")
+
