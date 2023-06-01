13cfa4f5fa1e46afb1a8db109035924d039144a8
kaue ochner <79806265+kakoch@users.noreply.github.com>
2023-06-01 13:25:12 -0300

    changing and arranging the weight field
    Changing and organizing the weight field.
    Some functions have changed. 
    For the next commit you should add a new button to calculate average weights.

--- cartao.js
+++ cartao.js
@@ -2,28 +2,36 @@ const array_cod = [
   {
     "lote": 225031,
     "tolerancia": 7,
-    "nome": 'Super 6 Plus',
-    "gramatura": 320,
+    "nome": 'Super labels',
+    "gramatura": 125,
   },
   {
     "lote": 218765,
     "tolerancia": 5,
-    "nome": 'CMPC RC',
-    "gramatura": 295,
+    "nome": 'Carton FC',
+    "gramatura": 293,
   },
   {
     "lote": 235868,
     "tolerancia": 2,
-    "nome": 'TP White Plus',
-    "gramatura": 275,
+    "nome": 'Tripe White',
+    "gramatura": 282,
   }
 ]
+
 
 // Botões
 const btn_8 = document.getElementById("i_8")
 const btn_10 = document.getElementById("i_10")
 const btn_13 = document.getElementById("i_13")
 const btn_proc = document.getElementById("btn_proc")
+//variaveis auxiliares
+let t = 1
+let k = 0
+let id = 1
+let resultado = []
+let array_result = []
+let retornoG = false;
 // Input procurar
 const codigo_proc = document.getElementById("p_cod")
 const nav1 = document.getElementById("nav1")
@@ -36,32 +44,26 @@ const nav_array = [...document.querySelectorAll("#nav2")]
 const g_mi = [...document.querySelectorAll("#g_mi")]
 const inputs = document.getElementsByClassName('campos_amostras')
 const li = document.getElementById('d_g_i')
+const divOnGramatures = document.getElementById(id)
 
-//variaveis auxiliares
-let t = 1
-let k = 0
-let id = 1
-let resultado = []
-let array_result = []
-let retornoG = false;
 // Funções
-//função remover os botões 8,10,13
-function removeBtns() {
-  q_amostra.remove()
-}
 //função criar elemento campos para gramatura
 function criarElemento() {
   const li = document.getElementById('d_g_i')
+  const div = document.createElement('div')
+  div.setAttribute("id", id)
+  div.setAttribute("class", "divGramatures")
   const novoElemento = document.createElement('input')
   const novoLabel = document.createElement('label',id)
   novoLabel.setAttribute("class", "label_amostras")
+  novoLabel.setAttribute("for", id)
   novoLabel.innerHTML=id
   novoElemento.setAttribute("type", "number")
   novoElemento.setAttribute("id", id)
   novoElemento.setAttribute("class", "campos_amostras")
-  li.appendChild(novoLabel)
-  li.appendChild(novoElemento)
-  t++
+  li.appendChild(div)
+  div.appendChild(novoLabel)
+  div.appendChild(novoElemento)
   id++
 }
 //função criar elemento campo quando codigo invalido
@@ -94,22 +96,23 @@ function creatGgmiTolerances() {
 }
 //função criar elemento campo quando codigo não existe
 function creatGgmiCodNotEx() {
-  
   g_mi.setAttribute("class", "descricaoTolerancia");
   g_mi.innerHTML = `O lote ${codigo_proc.value} não existe`;
   nav1.appendChild(g_mi);
   const div_to = document.getElementById("#g_mi")
 }
 
-function handleEmptyInput() {
+//função criar campo lote vazio
+/*function handleEmptyInput() {
   if (div_to == undefined) {
     creatGgmiInsira();
   } else {
     creatGgmiInsira();
     div_to.remove();
   }
-}
+}*/
 
+//função criar campo se encontrar lote
 function handleCodeFound() {
   const element = document.getElementById('g_mi'); 
   if (element) {
@@ -120,20 +123,17 @@ function handleCodeFound() {
   }
 }
 
+//função criar mensagem campo vazio 
 function handleCodeNotFound() {
-  const element = document.getElementById('g_mi'); 
-  if (!codigo_proc.value && element) {
-    element.remove();
+  const g_mi = document.getElementById('g_mi');
+  if (g_mi == undefined) {
     creatGgmiInsira()
-  }else if(!codigo_proc.value && !element){
-    creatGgmiInsira()
-  }else if (element) {
-    element.remove();
-    creatGgmiCodNotEx()
   }else{
-    creatGgmiCodNotEx()
+    g_mi.remove()
+    creatGgmiInsira()
   }
 }
+/*
 function aprov_reprov(){
   const div_n_g_mi = document.getElementById('n_g_mi'); 
   if (retornoG == true) {
@@ -156,6 +156,8 @@ function aprov_reprov(){
     }
   }
 }
+*/
+//função de procurar o numero do lote
 let itemVerificado = null;
 function procurar() {
   const numeroInput = document.getElementById('p_cod').value;
@@ -163,12 +165,13 @@ function procurar() {
   itemVerificado = array_cod.find(item => item.lote === numeroFornecidoPeloUsuario);
   if (itemVerificado) {
     handleCodeFound()
+    addButtonsGramature()
   } else {
     handleCodeNotFound()
   }
 }
-div_btns.addEventListener("click", (evt) => {
-  removeBtns()
+//função dar titulo no conjunto de gramaturas
+function addTitleButtonsGramature() {
   const t_g_mi = document.createElement('div');
   t_g_mi.setAttribute("class", "inspecao")
   t_g_mi.innerHTML=(`Insira a gramatura:`)
@@ -176,12 +179,19 @@ div_btns.addEventListener("click", (evt) => {
   g_i.setAttribute("id", "d_g_i");
   nav2.appendChild(t_g_mi)
   nav2.appendChild(g_i)
-  
-  while (t <= parseInt(evt.target.textContent)) {
-    criarElemento()
+}
+//função adicionar os campos de inserir gramatura
+function addButtonsGramature(){
+  const campos_amostras = [...document.getElementsByClassName('campos_amostras')]
+  if(campos_amostras == ''){
+    addTitleButtonsGramature()
+    for (let o = 0; o < 15; o++) {
+      criarElemento()
     }
-  })
+  }
   
+}
+//função calcular gramatura
   function calculateGramature() {
     var div = document.getElementById('nav2');
     var inputs = div.getElementsByClassName('campos_amostras');
@@ -206,24 +216,22 @@ div_btns.addEventListener("click", (evt) => {
     }
     return resultado;
   }
-  
-    function addButtonNextProcurar() {
-      const btn_proc = document.getElementById("btn_proc");
-      if (btn_proc) {
-        btn_proc.setAttribute("id", "aplic_gramature");
-        btn_proc.innerHTML = "Aplicar gramatura";
-        btn_proc.addEventListener("click", () => {
-            calculateGramature()
-        })
+  //if de teste perdido dando erro, mover e corrigir
+  if (btn_proc) {
+    btn_proc.addEventListener("click", () => {
+      procurar();
+      if(codigo_proc.value !== "") {
+        
       }
-    }
-    
+    });
+  }
+  /*function addButtonNextProcurar() {
+    const btn_proc = document.getElementById("btn_proc");
     if (btn_proc) {
+      btn_proc.setAttribute("id", "aplic_gramature");
+      btn_proc.innerHTML = "Aplicar gramatura";
       btn_proc.addEventListener("click", () => {
-        procurar();
-        if(codigo_proc.value !== "") {
-          addButtonNextProcurar();
-        }
-      });
+          calculateGramature()
+      })
     }
-    
+  }*/
